91d8d53c4a383b2fcd25d7ada1dae3b574351279
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-04-30 01:21:28 +0800

    use same service if cache key is the same

--- lib/active_model_cachers/cache_service_factory.rb
+++ lib/active_model_cachers/cache_service_factory.rb
@@ -3,24 +3,28 @@ require 'active_model_cachers/cache_service'
 
 module ActiveModelCachers
   class CacheServiceFactory
+    @key_class_mapping = {}
+
     class << self
       def create(reflect, cache_key, &query)
-        klass = Class.new(CacheService)
+        @key_class_mapping[cache_key] ||= ->{
+          klass = Class.new(CacheService)
+          
+          class << klass
+            def instance(id)
+              hash = (RequestStore.store[self] ||= {})
+              return hash[id] ||= new(id)
+            end
 
-        class << klass
-          def instance(id)
-            hash = (RequestStore.store[self] ||= {})
-            return hash[id] ||= new(id)
+            def [](id)
+              instance(id)
+            end
           end
 
-          def [](id)
-            instance(id)
-          end
-        end
-
-        klass.send(:define_method, :cache_key){ "#{cache_key}_#{@id}" }
-        klass.send(:define_method, :get_without_cache){ query.call(@id) }
-        return klass
+          klass.send(:define_method, :cache_key){ "#{cache_key}_#{@id}" }
+          klass.send(:define_method, :get_without_cache){ query.call(@id) }
+          next klass
+        }[]
       end
     end
   end
