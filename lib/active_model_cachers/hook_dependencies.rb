252ef534a0520e0075c46ea3704ad97240c843e7
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-05-01 16:38:53 +0800

    Add hooks when autoloading constants
    adjusting the code from
    https://github.com/mariovisic/rails/commit/b5ecbf36fa087c59e716e1e85eb05
    a5bcb658e29#diff-0082f4220cee92dcc93022aa9e45ed94R597

---
+++ lib/active_model_cachers/hook_dependencies.rb
@@ -0,0 +1,25 @@
+require 'active_support/dependencies'
+
+module ActiveSupport #:nodoc:
+  module Dependencies #:nodoc:
+    class << self
+      def onload(const_name, &blk)
+        load_hooks[const_name.to_sym].push(blk)
+      end
+
+      def clear_load_hooks
+        self.load_hooks = Hash.new([])
+      end
+
+      alias_method :new_constants_in_without_onload_hook, :new_constants_in if not method_defined?(:new_constants_in_without_onload_hook)
+      def new_constants_in(*descs, &block)
+        new_constants = new_constants_in_without_onload_hook(*descs, &block)
+        new_constants.each{|s| Dependencies.load_hooks[s].each{|hook| s.constantize.instance_exec(&hook) } }
+        return new_constants
+      end
+    end
+
+    mattr_accessor :load_hooks
+    clear_load_hooks
+  end
+end
