382c1f32a84c35a3c1bd3c2d64d831b3385b5105
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-05-01 17:16:46 +0800

    Fix type mismatch: const_name is string not symbol

--- lib/active_model_cachers/hook_dependencies.rb
+++ lib/active_model_cachers/hook_dependencies.rb
@@ -4,7 +4,7 @@ module ActiveSupport #:nodoc:
   module Dependencies #:nodoc:
     class << self
       def onload(const_name, &blk)
-        load_hooks[const_name.to_sym].push(blk)
+        load_hooks[const_name].push(blk)
       end
 
       def clear_load_hooks
@@ -14,7 +14,7 @@ module ActiveSupport #:nodoc:
       alias_method :new_constants_in_without_onload_hook, :new_constants_in if not method_defined?(:new_constants_in_without_onload_hook)
       def new_constants_in(*descs, &block)
         new_constants = new_constants_in_without_onload_hook(*descs, &block)
-        new_constants.each{|s| Dependencies.load_hooks[s].each{|hook| s.constantize.instance_exec(&hook) } }
+        new_constants.each{|s| load_hooks[s].each{|hook| s.constantize.instance_exec(&hook) } }
         return new_constants
       end
     end
