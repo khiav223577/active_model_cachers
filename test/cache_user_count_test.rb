45e91a23997e53730931cb883d4346c9d1ddef7a
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-05-04 18:54:09 +0800

    refactor

--- test/cache_user_count_test.rb
+++ test/cache_user_count_test.rb
@@ -1,27 +1,27 @@
 require 'base_test'
 
-class CacheSelfTest < BaseTest
+class CacheUserCountTest < BaseTest
   def test_basic_usage
 
-    assert_queries(1){ assert_equal 4, User.cacher_at(nil).count }
+    assert_queries(1){ assert_equal 4, User.cacher.count }
     assert_cache('active_model_cachers_User_at_count' => 4)
 
-    assert_queries(0){ assert_equal 4, User.cacher_at(nil).count }
+    assert_queries(0){ assert_equal 4, User.cacher.count }
     assert_cache('active_model_cachers_User_at_count' => 4)
   end
 
   def test_create
     user = nil
     
-    assert_queries(1){ assert_equal 4, User.cacher_at(nil).count }
-    assert_queries(0){ assert_equal 4, User.cacher_at(nil).count }
+    assert_queries(1){ assert_equal 4, User.cacher.count }
+    assert_queries(0){ assert_equal 4, User.cacher.count }
     assert_cache('active_model_cachers_User_at_count' => 4)
 
     user = User.create(id: -1)
     assert_cache({})
 
-    assert_queries(1){ assert_equal 5, User.cacher_at(nil).count }
-    assert_queries(0){ assert_equal 5, User.cacher_at(nil).count }
+    assert_queries(1){ assert_equal 5, User.cacher.count }
+    assert_queries(0){ assert_equal 5, User.cacher.count }
     assert_cache('active_model_cachers_User_at_count' => 5)
   ensure
     user.destroy if user
@@ -30,28 +30,28 @@ class CacheSelfTest < BaseTest
   def test_update_nothing
     user = User.find_by(name: 'John2')
 
-    assert_queries(1){ assert_equal 4, User.cacher_at(nil).count }
-    assert_queries(0){ assert_equal 4, User.cacher_at(nil).count }
+    assert_queries(1){ assert_equal 4, User.cacher.count }
+    assert_queries(0){ assert_equal 4, User.cacher.count }
     assert_cache('active_model_cachers_User_at_count' => 4)
 
     user.save
     assert_cache('active_model_cachers_User_at_count' => 4)
 
-    assert_queries(0){ assert_equal 4, User.cacher_at(nil).count }
+    assert_queries(0){ assert_equal 4, User.cacher.count }
     assert_cache('active_model_cachers_User_at_count' => 4)
   end
 
   def test_update
     user = User.find_by(name: 'John2')
 
-    assert_queries(1){ assert_equal 4, User.cacher_at(nil).count }
-    assert_queries(0){ assert_equal 4, User.cacher_at(nil).count }
+    assert_queries(1){ assert_equal 4, User.cacher.count }
+    assert_queries(0){ assert_equal 4, User.cacher.count }
     assert_cache('active_model_cachers_User_at_count' => 4)
 
     user.update_attributes(name: '??')
     assert_cache('active_model_cachers_User_at_count' => 4)
 
-    assert_queries(0){ assert_equal 4, User.cacher_at(nil).count }
+    assert_queries(0){ assert_equal 4, User.cacher.count }
     assert_cache('active_model_cachers_User_at_count' => 4)
   ensure
     user.update_attributes(name: 'John2')
@@ -60,15 +60,15 @@ class CacheSelfTest < BaseTest
   def test_destroy
     user = User.create
 
-    assert_queries(1){ assert_equal 5, User.cacher_at(nil).count }
-    assert_queries(0){ assert_equal 5, User.cacher_at(nil).count }
+    assert_queries(1){ assert_equal 5, User.cacher.count }
+    assert_queries(0){ assert_equal 5, User.cacher.count }
     assert_cache("active_model_cachers_User_at_count" => 5)
 
     user.destroy
     assert_cache({})
 
-    assert_queries(1){ assert_equal 4, User.cacher_at(nil).count }
-    assert_queries(0){ assert_equal 4, User.cacher_at(nil).count }
+    assert_queries(1){ assert_equal 4, User.cacher.count }
+    assert_queries(0){ assert_equal 4, User.cacher.count }
     assert_cache('active_model_cachers_User_at_count' => 4)
   ensure
     user.destroy
@@ -77,15 +77,15 @@ class CacheSelfTest < BaseTest
   def test_delete
     user = User.create
 
-    assert_queries(1){ assert_equal 5, User.cacher_at(nil).count }
-    assert_queries(0){ assert_equal 5, User.cacher_at(nil).count }
+    assert_queries(1){ assert_equal 5, User.cacher.count }
+    assert_queries(0){ assert_equal 5, User.cacher.count }
     assert_cache("active_model_cachers_User_at_count" => 5)
 
     user.delete
     assert_cache({})
 
-    assert_queries(1){ assert_equal 4, User.cacher_at(nil).count }
-    assert_queries(0){ assert_equal 4, User.cacher_at(nil).count }
+    assert_queries(1){ assert_equal 4, User.cacher.count }
+    assert_queries(0){ assert_equal 4, User.cacher.count }
     assert_cache('active_model_cachers_User_at_count' => 4)
   ensure
     user.destroy
