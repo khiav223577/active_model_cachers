bf1b3b220f9cec60a0c11da7ebd25505b266c2c8
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-05-04 18:51:12 +0800

    test update user

--- test/cache_user_count_test.rb
+++ test/cache_user_count_test.rb
@@ -27,6 +27,36 @@ class CacheSelfTest < BaseTest
     user.destroy if user
   end
 
+  def test_update_nothing
+    user = User.find_by(name: 'John2')
+
+    assert_queries(1){ assert_equal 4, User.cacher_at(nil).count }
+    assert_queries(0){ assert_equal 4, User.cacher_at(nil).count }
+    assert_cache('active_model_cachers_User_at_count' => 4)
+
+    user.save
+    assert_cache('active_model_cachers_User_at_count' => 4)
+
+    assert_queries(0){ assert_equal 4, User.cacher_at(nil).count }
+    assert_cache('active_model_cachers_User_at_count' => 4)
+  end
+
+  def test_update
+    user = User.find_by(name: 'John2')
+
+    assert_queries(1){ assert_equal 4, User.cacher_at(nil).count }
+    assert_queries(0){ assert_equal 4, User.cacher_at(nil).count }
+    assert_cache('active_model_cachers_User_at_count' => 4)
+
+    user.update_attributes(name: '??')
+    assert_cache('active_model_cachers_User_at_count' => 4)
+
+    assert_queries(0){ assert_equal 4, User.cacher_at(nil).count }
+    assert_cache('active_model_cachers_User_at_count' => 4)
+  ensure
+    user.update_attributes(name: 'John2')
+  end
+
   def test_destroy
     user = User.create
 
@@ -59,5 +89,9 @@ class CacheSelfTest < BaseTest
     assert_cache('active_model_cachers_User_at_count' => 4)
   ensure
     user.destroy
+  end
+
+  def test_destroyed_by_dependent_delete
+    # TODO
   end
 end
