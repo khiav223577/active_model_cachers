5bc5909502a136d83ce5981fe32615d6961a95cc
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-05-04 18:42:37 +0800

    custom expire should also clean cache on delete

--- lib/active_model_cachers.rb
+++ lib/active_model_cachers.rb
@@ -23,26 +23,21 @@ class << ActiveRecord::Base
 
   def cache_at(column, query = nil, expire_by: nil)
     service_klass = ActiveModelCachers::CacheServiceFactory.create_for_active_model(self, column, &query)
-    on_delete{|id| service_klass.instance(id).clean_cache }
     reflect = reflect_on_association(column)
-    if reflect
-      if reflect.options[:dependent] == :delete
-        after_commit ->{
-          target = association(column).load_target if destroyed? 
-          service_klass.instance(target.id).clean_cache if target
-        }
+    
+    if expire_by
+      ActiveSupport::Dependencies.onload(expire_by) do
+        on_delete{ service_klass.instance(nil).clean_cache }
+        after_commit ->{ service_klass.instance(nil).clean_cache if previous_changes.present? || destroyed? }
       end
+    elsif reflect
       ActiveSupport::Dependencies.onload(reflect.class_name) do
+        on_delete{|id| service_klass.instance(id).clean_cache }
         after_commit ->{ service_klass.instance(id).clean_cache if previous_changes.present? || destroyed? }
       end
     else
+      on_delete{|id| service_klass.instance(id).clean_cache }
       after_commit ->{ service_klass.instance(id).clean_cache if previous_changes.key?(column) || destroyed? }
-    end
-
-    if expire_by
-      ActiveSupport::Dependencies.onload(expire_by) do
-        after_commit ->{ service_klass.instance(nil).clean_cache if previous_changes.present? || destroyed? }
-      end
     end
   end
 
--- test/cache_user_count_test.rb
+++ test/cache_user_count_test.rb
@@ -39,7 +39,7 @@ class CacheSelfTest < BaseTest
 
     assert_queries(1){ assert_equal 4, User.cacher_at(nil).count }
     assert_queries(0){ assert_equal 4, User.cacher_at(nil).count }
-    assert_cache({})
+    assert_cache('active_model_cachers_User_at_count' => 4)
   ensure
     user.destroy
   end
@@ -56,7 +56,7 @@ class CacheSelfTest < BaseTest
 
     assert_queries(1){ assert_equal 4, User.cacher_at(nil).count }
     assert_queries(0){ assert_equal 4, User.cacher_at(nil).count }
-    assert_cache({})
+    assert_cache('active_model_cachers_User_at_count' => 4)
   ensure
     user.destroy
   end
