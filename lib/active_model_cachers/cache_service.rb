87eb5f47393c78e8fba46a6fc6b4b2527c61227c
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-05-06 02:16:34 +0800

    cache nil and false result

--- lib/active_model_cachers/cache_service.rb
+++ lib/active_model_cachers/cache_service.rb
@@ -1,3 +1,6 @@
+require 'active_model_cachers/nil_object'
+require 'active_model_cachers/false_object'
+
 module ActiveModelCachers
   class CacheService
     def initialize(id)
@@ -6,6 +9,7 @@ module ActiveModelCachers
 
     def get
       @cached_data ||= get_from_cache
+      return cache_to_raw_data(@cached_data)
     end
 
     def clean_cache
@@ -23,8 +27,22 @@ module ActiveModelCachers
       fail 'not implement'
     end
 
+    def raw_to_cache_data(raw)
+      return NilObject if raw == nil
+      return FalseObject if raw == false
+      return raw
+    end
+
+    def cache_to_raw_data(cached_data)
+      return nil if cached_data == NilObject
+      return false if cached_data == FalseObject
+      return cached_data
+    end
+
     def get_from_cache
-      ActiveModelCachers.config.store.fetch(cache_key, expires_in: 30.minutes){ get_without_cache }
+      ActiveModelCachers.config.store.fetch(cache_key, expires_in: 30.minutes) do 
+        raw_to_cache_data(get_without_cache)
+      end
     end
   end
 end
