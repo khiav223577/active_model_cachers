3539d3e11c527913e9b893d6d5d628c48e447131
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-05-06 02:37:41 +0800

    test false data

---
+++ test/cache_false_data_test.rb
@@ -0,0 +1,20 @@
+require 'base_test'
+
+class CacheFalseDataTest < BaseTest
+  def test_basic_usage
+    user = User.find_by(name: 'John1')
+
+    assert_queries(1){ assert_equal true, User.cacher_at(user.id).has_post? }
+    assert_queries(0){ assert_equal true, User.cacher_at(user.id).has_post? }
+    assert_cache('active_model_cachers_User_at_has_post?_1' => true)
+  end
+
+  def test_false_data_should_be_cached
+    user = User.find_by(name: 'John4')
+
+    # false data should be cached, no more query is called if it is cached.
+    assert_queries(1){ assert_equal false, User.cacher_at(user.id).has_post? }
+    assert_queries(0){ assert_equal false, User.cacher_at(user.id).has_post? }
+    assert_cache('active_model_cachers_User_at_has_post?_4' => ActiveModelCachers::FalseObject)
+  end
+end
