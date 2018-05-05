0629381954ea84b8abdc66c1900e575f20c8b63e
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-05-06 01:15:02 +0800

    refactoring hook_dependencies

--- lib/active_model_cachers/hook_dependencies.rb
+++ lib/active_model_cachers/hook_dependencies.rb
@@ -1,30 +1,27 @@
 require 'active_support/dependencies'
 
-module ActiveSupport #:nodoc:
-  module Dependencies #:nodoc:
-    class << self
-      def onload(const_name, &block)
-        const = const_name if not const_name.is_a?(String)
-        if const or Module.const_defined?(const_name)
-          (const || const_name.constantize).instance_exec(&block)
-        else
-          load_hooks[const_name].push(block)
-        end
-      end
-
-      def clear_load_hooks
-        self.load_hooks = Hash.new{|h, k| h[k] = [] }
-      end
-
-      alias_method :new_constants_in_without_onload_hook, :new_constants_in if not method_defined?(:new_constants_in_without_onload_hook)
-      def new_constants_in(*descs, &block)
-        new_constants = new_constants_in_without_onload_hook(*descs, &block)
-        new_constants.each{|s| load_hooks[s].each{|hook| s.constantize.instance_exec(&hook) } }
-        return new_constants
-      end
+module ActiveModelCachers::HookDepdenencies
+  def onload(const_name, &block)
+    const = const_name if not const_name.is_a?(String)
+    if const or Module.const_defined?(const_name)
+      (const || const_name.constantize).instance_exec(&block)
+    else
+      load_hooks[const_name].push(block)
     end
+  end
 
-    mattr_accessor :load_hooks
-    clear_load_hooks
+  def load_hooks
+    @load_hooks ||= Hash.new{|h, k| h[k] = [] }
+  end
+
+  def clear_load_hooks
+    @load_hooks = Hash.new{|h, k| h[k] = [] }
+  end
+
+  def new_constants_in(*)
+    new_constants = super.each{|s| load_hooks[s].each{|hook| s.constantize.instance_exec(&hook) } }
+    return new_constants
   end
 end
+
+ActiveSupport::Dependencies.send(:extend, ActiveModelCachers::HookDepdenencies)
