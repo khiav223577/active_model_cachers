79a21d2ae9610325a4acbe8d373564b7f3072c09
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-05-07 01:45:31 +0800

    test update

--- test/cache_at_has_many_test.rb
+++ test/cache_at_has_many_test.rb
@@ -31,4 +31,42 @@ class CacheAtHasManyTest < BaseTest
   ensure
     new_post.delete if new_post
   end
+
+  # ----------------------------------------------------------------
+  # ● Update
+  # ----------------------------------------------------------------
+  def test_update_nothing
+    user = User.find_by(name: 'John4')
+    post = Post.create(id: -1, user: user)
+
+    assert_queries(1){ assert_equal [post], User.cacher_at(user.id).posts }
+    assert_queries(0){ assert_equal [post], User.cacher_at(user.id).posts }
+    assert_cache('active_model_cachers_User_at_posts_4' => [post])
+
+    post.save
+    assert_cache('active_model_cachers_User_at_posts_4' => [post])
+
+    assert_queries(0){ assert_equal [post], User.cacher_at(user.id).posts }
+    assert_cache('active_model_cachers_User_at_posts_4' => [post])
+  ensure
+    post.delete
+  end
+
+  def test_update_title
+    user = User.find_by(name: 'John4')
+    post = Post.create(id: -1, user: user)
+
+    assert_queries(1){ assert_equal [post], User.cacher_at(user.id).posts }
+    assert_queries(0){ assert_equal [post], User.cacher_at(user.id).posts }
+    assert_cache('active_model_cachers_User_at_posts_4' => [post])
+
+    post.update_attributes(title: '學生退出校園')
+    assert_cache({})
+
+    assert_queries(1){ assert_equal [post], User.cacher_at(user.id).posts }
+    assert_queries(0){ assert_equal [post], User.cacher_at(user.id).posts }
+    assert_cache('active_model_cachers_User_at_posts_4' => [post])
+  ensure
+    post.delete
+  end
 end
