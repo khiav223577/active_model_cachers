af5eab35eca4501c5ded0dc4c0483646a3cc4d6b
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-05-06 16:49:10 +0800

    add attr_model to wrap reflect, klass, and column

---
+++ lib/active_model_cachers/active_record/attr_model.rb
@@ -0,0 +1,44 @@
+module ActiveModelCachers
+  module ActiveRecord
+    class AttrModel
+      attr_reader :klass, :column, :reflect
+
+      def initialize(klass, column)
+        @klass = klass
+        @column = column
+        @reflect = klass.reflect_on_association(column)
+      end
+
+      def association?
+        return (@reflect != nil)
+      end
+
+      def class_name
+        return if not association?
+        return @reflect.class_name
+      end
+
+      def belongs_to?
+        return false if not association?
+        return @reflect.belongs_to?
+      end
+
+      def primary_key
+        return if not association?
+        return (@reflect.belongs_to? ? @reflect.klass : @reflect.active_record).primary_key
+      end
+
+      def foreign_key(reverse: false)
+        return if not association?
+        # key may be symbol if specify foreign_key in association options
+        return @reflect.chain.last.foreign_key.to_s if reverse
+        return (@reflect.belongs_to? == reverse ? primary_key : @reflect.foreign_key).to_s
+      end
+
+      def query_model(id)
+        id = @reflect.active_record.where(id: id).limit(1).pluck(foreign_key).first if foreign_key != 'id'
+        return id ? @reflect.klass.find_by(primary_key => id) : nil
+      end
+    end
+  end
+end
--- lib/active_model_cachers/active_record/extension.rb
+++ lib/active_model_cachers/active_record/extension.rb
@@ -1,3 +1,5 @@
+require 'active_model_cachers/active_record/attr_model'
+
 module ActiveModelCachers
   module ActiveRecord
     module Extension
@@ -6,9 +8,10 @@ module ActiveModelCachers
       end
 
       def cache_at(column, query = nil, expire_by: nil, on: nil, foreign_key: :id)
-        service_klass, with_id = ActiveModelCachers::CacheServiceFactory.create_for_active_model(self, column, &query)
+        attr = AttrModel.new(self, column)
+        service_klass, with_id = ActiveModelCachers::CacheServiceFactory.create_for_active_model(attr, &query)
 
-        expire_by ||= reflect_on_association(column).try(:class_name) || "#{self}##{column}"
+        expire_by ||= attr.association? ? attr.class_name : "#{self}##{column}"
         class_name, column = expire_by.split('#', 2)
 
         define_callback_for_cleaning_cache(class_name, column, foreign_key, on: on) do |id|
@@ -17,7 +20,8 @@ module ActiveModelCachers
       end
 
       def has_cacher?(column = nil)
-        ActiveModelCachers::CacheServiceFactory.has_cacher?(self, column)
+        attr = AttrModel.new(self, column)
+        return ActiveModelCachers::CacheServiceFactory.has_cacher?(attr)
       end
 
       private
