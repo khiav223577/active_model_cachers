d3b817d18bab3bf9cc9dfc430f187a0a266107a5
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-05-08 02:35:57 +0800

    pass model to instance cacher

--- lib/active_model_cachers/active_record/cacher.rb
+++ lib/active_model_cachers/active_record/cacher.rb
@@ -21,15 +21,16 @@ module ActiveModelCachers
         def create_cacher_klass_at(target)
           cacher_klass = Class.new(self)
           cacher_klass.define_singleton_method(:attributes){ @attributes ||= [] }
-          target.define_singleton_method(:cacher_at){|id| cacher_klass.new(id) }
+          target.define_singleton_method(:cacher_at){|id| cacher_klass.new(id: id) }
           target.define_singleton_method(:cacher){ cacher_klass.new }
-          target.send(:define_method, :cacher){ cacher_klass.new(id) }
+          target.send(:define_method, :cacher){ cacher_klass.new(model: self) }
           return cacher_klass
         end
       end
 
-      def initialize(id = nil)
-        @id = id
+      def initialize(id: nil, model: nil)
+        @id = model ? model.id : id
+        @model = model
       end
 
       private
