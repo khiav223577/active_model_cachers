0e8e2efaffec43c4583b0a5654c1e09f355a0a48
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-05-18 17:10:40 +0800

    fix warnings

--- test/cache_email_valid_test.rb
+++ test/cache_email_valid_test.rb
@@ -3,8 +3,6 @@ require 'base_test'
 
 class CacheEmailValidTest < BaseTest
   def test_basic_usage
-    user = User.find_by(name: 'John2')
-
     assert_queries(1){ assert_equal true, User.cacher_at('john2@example.com').email_valid? }
     assert_queries(0){ assert_equal true, User.cacher_at('john2@example.com').email_valid? }
     assert_cache('active_model_cachers_User_at_email_valid?_john2@example.com' => true)
