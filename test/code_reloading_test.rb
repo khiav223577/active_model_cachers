011ec7ae2113f6fb7cafca083f6f685d76eb83f1
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-06-01 01:45:21 +0800

    test reloading other associations

--- test/code_reloading_test.rb
+++ test/code_reloading_test.rb
@@ -2,7 +2,7 @@
 require 'base_test'
 
 class CodeReloadingTest < BaseTest
-  def test_basic_usage
+  def test_self
     profile1 = Profile.find_by(token: 'tt9wav')
 
     origin_profile_klass = Profile
@@ -19,6 +19,26 @@ class CodeReloadingTest < BaseTest
     if origin_profile_klass
       Object.send(:remove_const, :Profile)
       Object.send(:const_set, :Profile, origin_profile_klass)
+    end
+  end
+
+  def test_association
+    posts1 = User.first.posts
+
+    origin_post_klass = Post
+    Object.send(:remove_const, :Post)
+    load 'lib/models/post.rb'
+
+    posts2 = User.first.posts
+
+    assert_queries(1){ assert_equal 3, User.cacher_at(1).posts.size }
+    assert_queries(0){ assert_equal 3, User.cacher_at(1).posts.size }
+    assert_cache('active_model_cachers_User_at_posts_1' => posts2)
+    refute_equal(posts1, posts2)
+  ensure
+    if origin_post_klass
+      Object.send(:remove_const, :Post)
+      Object.send(:const_set, :Post, origin_post_klass)
     end
   end
 end
