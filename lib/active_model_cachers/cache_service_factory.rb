e59dadde0f194ad58153b48a6a379a7812ab4c78
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-06-01 01:32:54 +0800

    rename method name to clarify it

--- lib/active_model_cachers/cache_service_factory.rb
+++ lib/active_model_cachers/cache_service_factory.rb
@@ -15,7 +15,7 @@ module ActiveModelCachers
       def create_for_active_model(attr, query, current_klass)
         class_name, _ = attr.extract_class_and_column
         cache_key = get_cache_key(attr)
-        clean_klass_cache_after_reloaded!(cache_key, current_klass) if current_klass.name == class_name.to_s
+        clean_klass_cache_if_reloaded!(cache_key, current_klass) if current_klass.name == class_name.to_s
         return create(cache_key, query)
       end
 
@@ -39,7 +39,7 @@ module ActiveModelCachers
         return "active_model_cachers_#{class_name}"
       end
 
-      def clean_klass_cache_after_reloaded!(cache_key, current_klass)
+      def clean_klass_cache_if_reloaded!(cache_key, current_klass)
         origin_klass, @cache_key_klass_mapping[cache_key] = @cache_key_klass_mapping[cache_key], current_klass
         @key_class_mapping[cache_key] = nil if origin_klass and origin_klass != current_klass # when code reloaded in development.
       end
