83431859911d32f8c9f9500f883b92598084ca58
Rumble Huang <mrtmrt15xn@yahoo.com.tw>
2019-01-13 07:00:40 +0800

    test create

--- test/cache_bool_data_of_has_and_belongs_to_many_test.rb
+++ test/cache_bool_data_of_has_and_belongs_to_many_test.rb
@@ -9,4 +9,43 @@ class CacheBoolDataOfHasAndBelongsToManyTest < BaseTest
     assert_queries(0){ assert_equal true, user.cacher.has_achievements_by_belongs_to_many? }
     assert_cache('active_model_cachers_User_at_has_achievements_by_belongs_to_many?_1' => true)
   end
+
+  # ----------------------------------------------------------------
+  # ● Create
+  # ----------------------------------------------------------------
+  def test_create
+    user = User.find_by(name: 'John4')
+    achievement = Achievement.take
+
+    assert_queries(1){ assert_equal false, user.cacher.has_achievements_by_belongs_to_many? }
+    assert_queries(0){ assert_equal false, user.cacher.has_achievements_by_belongs_to_many? }
+    assert_cache('active_model_cachers_User_at_has_achievements_by_belongs_to_many?_4' => ActiveModelCachers::FalseObject)
+
+    assert_queries(1){ UserAchievement.create(user: user, achievement: achievement) }
+    assert_cache({})
+
+    assert_queries(1){ assert_equal true, user.cacher.has_achievements_by_belongs_to_many? }
+    assert_queries(0){ assert_equal true, user.cacher.has_achievements_by_belongs_to_many? }
+    assert_cache('active_model_cachers_User_at_has_achievements_by_belongs_to_many?_4' => true)
+  ensure
+    user.user_achievements.delete_all
+  end
+
+  def test_create_by_pushing
+    user = User.find_by(name: 'John4')
+    achievement = Achievement.take
+
+    assert_queries(1){ assert_equal false, user.cacher.has_achievements_by_belongs_to_many? }
+    assert_queries(0){ assert_equal false, user.cacher.has_achievements_by_belongs_to_many? }
+    assert_cache('active_model_cachers_User_at_has_achievements_by_belongs_to_many?_4' => ActiveModelCachers::FalseObject)
+
+    assert_queries(1){ user.achievements_by_belongs_to_many << achievement }
+    assert_cache({})
+
+    assert_queries(1){ assert_equal true, user.cacher.has_achievements_by_belongs_to_many? }
+    assert_queries(0){ assert_equal true, user.cacher.has_achievements_by_belongs_to_many? }
+    assert_cache('active_model_cachers_User_at_has_achievements_by_belongs_to_many?_4' => true)
+  ensure
+    user.user_achievements.delete_all
+  end
 end
--- test/cache_bool_data_of_has_many_through_test.rb
+++ test/cache_bool_data_of_has_many_through_test.rb
@@ -9,4 +9,43 @@ class CacheBoolDataOfHasManyThroughTest < BaseTest
     assert_queries(0){ assert_equal true, user.cacher.has_achievements? }
     assert_cache('active_model_cachers_User_at_has_achievements?_1' => true)
   end
+
+  # ----------------------------------------------------------------
+  # ● Create
+  # ----------------------------------------------------------------
+  def test_create
+    user = User.find_by(name: 'John4')
+    achievement = Achievement.take
+
+    assert_queries(1){ assert_equal false, user.cacher.has_achievements? }
+    assert_queries(0){ assert_equal false, user.cacher.has_achievements? }
+    assert_cache('active_model_cachers_User_at_has_achievements?_4' => ActiveModelCachers::FalseObject)
+
+    assert_queries(1){ UserAchievement.create(user: user, achievement: achievement) }
+    assert_cache({})
+
+    assert_queries(1){ assert_equal true, user.cacher.has_achievements? }
+    assert_queries(0){ assert_equal true, user.cacher.has_achievements? }
+    assert_cache('active_model_cachers_User_at_has_achievements?_4' => true)
+  ensure
+    user.user_achievements.delete_all
+  end
+
+  def test_create_by_pushing
+    user = User.find_by(name: 'John4')
+    achievement = Achievement.take
+
+    assert_queries(1){ assert_equal false, user.cacher.has_achievements? }
+    assert_queries(0){ assert_equal false, user.cacher.has_achievements? }
+    assert_cache('active_model_cachers_User_at_has_achievements?_4' => ActiveModelCachers::FalseObject)
+
+    assert_queries(1){ user.achievements << achievement }
+    assert_cache({})
+
+    assert_queries(1){ assert_equal true, user.cacher.has_achievements? }
+    assert_queries(0){ assert_equal true, user.cacher.has_achievements? }
+    assert_cache('active_model_cachers_User_at_has_achievements?_4' => true)
+  ensure
+    user.user_achievements.delete_all
+  end
 end
