2de860ccb1724a06eca55b6007dc17134724c2bc
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2019-09-23 17:09:54 +0800

    Clear aggregation cache only if model uses aggregations
    Aggregations are now loaded lazily. See: rails/rails@657060b#diff-391caa9b9464021e932ebf657fa9b13c, and https://github.com/zdennis/activerecord-import/pull/623

--- lib/active_model_cachers/cache_service.rb
+++ lib/active_model_cachers/cache_service.rb
@@ -137,7 +137,7 @@ module ActiveModelCachers
     def clean_ar_cache(models)
       return if not models.first.is_a?(::ActiveRecord::Base)
       models.each_with_index do |model, index|
-        model.send(:clear_aggregation_cache)
+        model.send(:clear_aggregation_cache) if model.respond_to?(:clear_aggregation_cache, true)
         model.send(:clear_association_cache)
       end
     end
